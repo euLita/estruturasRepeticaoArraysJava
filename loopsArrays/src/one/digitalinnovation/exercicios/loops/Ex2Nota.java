4720608640de093995086ae9672df3af45837a7f
euLita <litas.educ@gmail.com>
2022-07-25 22:18:27 -0400

    second exercise of the course, loops method

---
+++ loopsArrays/src/one/digitalinnovation/exercicios/loops/Ex2Nota.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.exercicios.loops;
+import java.util.Scanner;
+/**
+ *  Faça um programa que peça uma nota, entre 0(zero) e 10(dez).
+ *  Exiba uma mensagem caso o valor seja inválido e continue pedindo
+ *      até que o usuário informe um valor válido.
+ */
+public class Ex2Nota {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int nota;
+        System.out.println(" Nota: ");
+        nota = scan.nextInt();
+
+        while (nota < 0 || nota > 10) {
+            System.out.println(" Nota Inválida! Digite novamente: ");
+            nota = scan.nextInt();
+        }
+    }
+}
